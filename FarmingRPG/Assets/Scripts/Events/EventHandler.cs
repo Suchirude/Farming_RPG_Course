1723c144ab30ea60a3575b6eb57c70647c49f5fb
Suchirude <ole.schilde@gmail.com>
2021-12-12 20:53:00 +0200

    Test Harness and previous

---
+++ FarmingRPG/Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,35 @@
+public delegate void MovementDelegate(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect, 
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight);
+
+public static class EventHandler
+{
+    //Movement Event
+
+    public static event MovementDelegate MovementEvent;
+
+    public static void CallMovementEvent(float inputX, float inputY, bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleUp, bool idleDown, bool idleLeft, bool idleRight)
+    {
+        if(MovementEvent != null)
+        {
+            MovementEvent(inputX, inputY, 
+                isWalking, isRunning, isIdle, isCarrying,
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                idleUp, idleDown, idleLeft, idleRight);
+        }
+    }
+}
